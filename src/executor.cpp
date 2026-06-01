704363ad6c26912329c531373ba863be6c5a1547
filipeparedes <filipeparedes3@gmail.com>
2026-06-01 20:57:15 +0100

    feat: add io redirection to external binaries

--- src/executor.cpp
+++ src/executor.cpp
@@ -18,6 +18,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <csignal>
+#include <fcntl.h>
 
 int Executor::execute(const cppsh::Command& cmd) {
     if (cmd.args.empty()) return 0;
@@ -47,6 +48,34 @@ int Executor::execute(const cppsh::Command& cmd) {
         signal(SIGTSTP, SIG_DFL);
 
         std::vector<char*> argv = cppsh::to_vchar(cmd.args);
+
+        //Input redirection
+        if (!cmd.input_file.empty()) {
+            //get file descriptor for the input file
+            int fd = open(cmd.input_file.c_str(), O_RDONLY);
+
+            //redirect stdin to the input file
+            dup2(fd, STDIN_FILENO);
+
+            //close the file
+            close(fd);
+        }
+
+        //Output redirection
+        if (!cmd.output_file.empty()) {
+            //define if it overwrites (truncates) or appends
+            int flags = cmd.append ? O_WRONLY | O_CREAT | O_APPEND 
+                                : O_WRONLY | O_CREAT | O_TRUNC;
+            
+            //get file descriptor for the output file
+            int fd = open(cmd.output_file.c_str(), flags, 0644);
+
+            //redirect stdout to the input file
+            dup2(fd, STDOUT_FILENO);
+
+            //close the file
+            close(fd);
+        }
 
         //.data() converts std::vector<char*> into char**
         execvp(cmd.args[0].c_str(), argv.data());

59e3461ae4061d432cb51e6136ea75b96e295c66
filipeparedes <filipeparedes3@gmail.com>
2026-06-01 19:10:36 +0100

    feat: add io redirection check to parser

--- lib/parser.cpp
+++ lib/parser.cpp
@@ -4,8 +4,8 @@
  * 
  * @author Filipe Paredes (filipeparedes3@gmail.com)
  * 
- * @version 0.1
- * @date 2026-04-30
+ * @version 0.2.0
+ * @date 2026-06-01
  * 
  * @copyright Copyright (c) 2026
  * 
@@ -18,10 +18,41 @@
 namespace cppsh {
 
     cppsh::Command Parser::parse(const std::string& input) const {
-        std::vector<std::string> tokenized_str = tokenize(input);
-
+        std::vector<std::string> tok_vec = tokenize(input);
         Command cmd;
-        cmd.args = tokenized_str;
+
+        // IO redirection
+        for (int i = 0; i<tok_vec.size(); i++) {
+            //Input Redirection
+            if (tok_vec[i] == "<"){
+                cmd.input_file = tok_vec[i + 1]; // next argument should be the file name
+
+                tok_vec.erase(tok_vec.begin() + i); //erase redirection operator
+                tok_vec.erase(tok_vec.begin() + i); //shifted down, erase file name
+
+                i--;
+            } 
+            //Output Redirection (Append)
+            else if (tok_vec[i] == ">>"){
+                cmd.output_file = tok_vec[i + 1];
+                cmd.append = true; // >> appends instead of overwriting
+
+                tok_vec.erase(tok_vec.begin() + i);
+                tok_vec.erase(tok_vec.begin() + i);
+
+                i--;
+            }
+            //Output Redirection (Overwrite)
+            else if (tok_vec[i] == ">"){
+                cmd.output_file = tok_vec[i + 1];
+
+                tok_vec.erase(tok_vec.begin() + i);
+                tok_vec.erase(tok_vec.begin() + i);
+
+                i--;
+            }
+        }
+        cmd.args = tok_vec;
 
         return cmd;
     }
@@ -31,7 +62,7 @@ namespace cppsh {
         std::stringstream stream(input);
         std::string token;
 
-        //get each token from the strea
+        //get each token from the stream
         while (stream >> token){
             tokens.push_back(token);
         }

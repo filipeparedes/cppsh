30e476f1892081a5ed2578844f19c5b63eb39818
filipeparedes <filipeparedes3@gmail.com>
2026-08-29 22:13:46 +0100

    feat: add script file execution via cli arguments

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,20 @@
  * Starts the main shell loop.
  * 
  * @author Filipe Paredes (filipeparedes3@gmail.com)
- * @version 0.2.0
- * @date 2026-06-20
+ * @version 1.0.0
+ * @date 2026-08-29
  * 
  * @copyright Copyright (c) 2026
  * 
  */
 
  import cppsh.shell;
+ import cppsh.scripting;
 
- int main() {
-   auto res = run();
-   if (!res)
+ int main(int argc, char* argv[]) {
+    auto res = (argc == 2) ? execute_file(argv[1]) : run();
+
+    if (!res)
       return 1;
-   return 0;
+    return 0;
  }
